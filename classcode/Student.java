52f05fd7040840dd06433e3e8fd943a4f9a35e71
raymond0809 <raymondschool89@gmail.com>
2022-02-16 08:15:49 -0500

    updated

---
+++ classcode/Student.java
@@ -0,0 +1,58 @@
+
+public class Student {
+  // fields, instance varaibles (data)
+  // each instance of the Student class gets a copy of each variable
+  private String id;
+  private String name;
+  private boolean closeContact;
+  private int[] grades;
+
+  // constuctor method
+  public Student(String id, String n, boolean cc, int[] g) {
+    this.id = id;
+    this.name = n;
+    this.closeContact = cc;
+    this.grades = g;
+  }
+
+  // alternate constructor (overloaded constructor)
+  public Student(String id, String n, int[] g) {
+    this.id = id;
+    this.name = n;
+    this.closeContact = false;
+    this.grades = g;
+  }
+
+  // behaviors (methods)
+
+  // get the close contact status of the student
+  public boolean getCloseContact() {
+    return this.closeContact;
+  } // end isCloseContact method
+
+  // set the value of close contact
+  public void setCloseContact(boolean value) {
+    this.closeContact = value;
+  }
+
+  // see if the student is failing
+  public boolean isFailing() {
+
+    return false;
+  }
+
+  // get object's name value
+  public String getName() {
+    return this.name;
+  }
+
+  public double getAverage() {
+    int total = 0;
+
+    for (int grade : this.grades) {
+      total += grade;
+    } // end for-each loop
+    return total / grades.length;
+  } // end getAverage method
+
+}
